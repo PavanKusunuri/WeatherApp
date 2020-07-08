ff8c362c4c2dd03b09687b785e1734b67a95f8fc
Pavan <pavantejakusunuri@gmail.com>
2020-07-08 08:22:03 +0530

    Added Object Destructuring methods

--- utils/geocode.js
+++ utils/geocode.js
@@ -6,17 +6,17 @@ const geocode = (address, callback) => {
     address +
     ".json?access_token=pk.eyJ1IjoicGF2YW4tMTciLCJhIjoiY2tjMnBvcHd5MjV5dDM0bXgyNWE5YjRuYiJ9.D0Xfg3FFlPDABvifpMBidQ&limit=1";
 
-  request({ url: url, json: true }, (error, response) => {
-    console.log("Response" + response);
+  request({ url, json: true }, (error, { body }) => {
+    // console.log("Response" + response);
     if (error) {
       callback("Unable to connect to location services", undefined);
-    } else if (response.body.features.length === 0) {
+    } else if (body.features.length === 0) {
       callback("Unable to find location.Try another search", undefined);
     } else {
       callback(undefined, {
-        latitude: response.body.features[0].center[1],
-        longitude: response.body.features[0].center[0],
-        location: response.body.features[0].place_name,
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name,
       });
     }
   });
